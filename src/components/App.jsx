7d972e7c797afd5a3233c4408e585b5ee840ecc0
storycoding <youknownuno@gmail.com>
2017-12-13 19:37:04 -0800

    completed more of static methods

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,44 +6,14 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
+        <VideoPlayer videoObject={window.exampleVideoData[0]} />
       </div>
       <div className="col-md-5">
+        <VideoList videoArray={window.exampleVideoData} />
       </div>
     </div>
 
-    <VideoPlayer videoObject={{
-      kind: 'youtube#searchResult',
-      etag: 'abQHWywil_AkNqdqji7_FqiK-u4/Ykxo_CqKu8F8kcm-iNgL332gQTY',
-      id: {
-        kind: 'youtube#video',
-        videoId: '4ZAEBxGipoA'
-      },
-      snippet: {
-        publishedAt: '2015-08-02T20:52:58.000Z',
-        channelId: 'UCJbPGzawDH1njbqV-D5HqKw',
-        title: 'React JS Tutorial for Beginners - 1 - Introduction',
-        description: 'My website - https://www.thenewboston.com/videos.php Have questions about the tutorial or React? Ask them here ...',
-        thumbnails: {
-          default: {
-            url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/default.jpg',
-            width: 120,
-            height: 90
-          },
-          medium: {
-            url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/mqdefault.jpg',
-            width: 320,
-            height: 180
-          },
-          high: {
-            url: 'https://i.ytimg.com/vi/4ZAEBxGipoA/hqdefault.jpg',
-            width: 480,
-            height: 360
-          }
-        },
-        channelTitle: 'thenewboston',
-        liveBroadcastContent: 'none'
-      }
-    }} />
+    
   </div>
 );
 
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,38 @@
-var VideoList = () => {
-  return (
-    <div className="video-list">
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-      <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    </div>
-  );
+class VideoList extends React.Component {
 
-  //some method to render ()
+  constructor (props) {
+    super(props);
+  }
 
-};
+  render (props) {
+    
+    console.log('this = ' + this);
+
+    for (var i = 1; i <= 5; i++) {
+      let listItem = this.props.videoArray[i];
+      this['video' + i] = (<VideoListEntry videoArrayObject={listItem} />);
+    }
+   
+
+    var list = (
+      <div className="video-list">
+        <div>{this.video1}</div>
+        <div>{this.video2}</div>
+        <div>{this.video3}</div>
+        <div>{this.video4}</div>
+        <div>{this.video5}</div>
+      </div>
+    );    
+
+    return list;   
+  }
+  
+}
+  
+
+
+
+
 
 //create the render method
   //iterate the first 5 results from exampleVideoData
@@ -23,9 +44,9 @@ var VideoList = () => {
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
-VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
-};
+// VideoList.propTypes = {
+//   videos: React.PropTypes.array.isRequired
+// };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
