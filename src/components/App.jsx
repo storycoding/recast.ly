09800fbf5e4d5ea2876e304ea9bdb5073b9d8a45
storycoding <youknownuno@gmail.com>
2017-12-13 17:46:32 -0800

    Began recastly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,9 +10,11 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
       </div>
     </div>
+
+    <VideoList videos={window.exampleVideoData} /> 
+
   </div>
 );
 
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,22 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = () => {
+  return (
+    <div className="video-list">
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    </div>
+  );
+};
+
+//create the render method
+  //iterate the first 5 results from exampleVideoData
+    // render those elements into videoListEntry react instances 
+      //append the results to videoList
+  // return the videoList
+
+
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -17,3 +27,6 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+
+//src/data/exampleVideoData.js
